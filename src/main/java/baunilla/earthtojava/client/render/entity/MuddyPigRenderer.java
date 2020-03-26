ec013180433f195173fabaa542a5ee60a237dbf3
StalckGx <vinny.polsom@outlook.com>
2020-03-26 16:02:30 -0300

    v0.7.2 Quick fix * MuddyPigRenderer is now client only * Changed muddy pig texture path

--- src/main/java/baunilla/earthtojava/client/render/entity/MuddyPigRenderer.java
+++ src/main/java/baunilla/earthtojava/client/render/entity/MuddyPigRenderer.java
@@ -2,10 +2,13 @@ package baunilla.earthtojava.client.render.entity;
 
 import baunilla.earthtojava.client.model.entity.MuddyPigModel;
 import baunilla.earthtojava.common.entity.MuddyPigEntity;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.minecraft.client.render.entity.EntityRenderDispatcher;
 import net.minecraft.client.render.entity.MobEntityRenderer;
 import net.minecraft.util.Identifier;
 
+@Environment(EnvType.CLIENT)
 public class MuddyPigRenderer extends MobEntityRenderer<MuddyPigEntity, MuddyPigModel<MuddyPigEntity>> {
 
     public MuddyPigRenderer(EntityRenderDispatcher renderManager) {
@@ -14,7 +17,7 @@ public class MuddyPigRenderer extends MobEntityRenderer<MuddyPigEntity, MuddyPig
 
     @Override
     public Identifier getTexture(MuddyPigEntity entity) {
-        return new Identifier("earthtojava:textures/entity/muddy_pig/muddy_pig.png");
+        return new Identifier("earthtojava:textures/entity/pig/muddy_pig.png");
     }
     
 }
